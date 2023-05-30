15fcf71e0c2975a1f2d7eb22c08f43b186742f0d
Gustavo P R Mattos <gustavo.prm6@gmail.com>
2023-05-30 08:39:53 -0300

    incluída a programação do botão Listar

--- js/brinquedoteca.js
+++ js/brinquedoteca.js
@@ -4,19 +4,34 @@
 const criancas=[]//variável global
 
 frm.addEventListener("submit", (e)=>{
-    e.preventDefault
+    e.preventDefault()
 
     const nome=frm.inNome.value// pega os valores dos nomes enviados ao form
     const idade=Number(frm.inIdade.value) //pega os valores das idades enviados ao form
     criancas.push({nome,idade})// add dados ao vetor 
-    frm.reset // limpa os campos
+    frm.reset() // limpa os campos
     frm.inNome.focus() // posiciona o cursor no campo
-    frm.btListar.dispatchEvent(new Event ("click"))
-
-})
-
-frm.btListar,addEventListener("click",()=>{
-
     
 
 })
+
+frm.btListar.addEventListener("click",()=>{
+  
+    if(criancas.length==0){ // verifica se o vetor está vazio
+        alert("Não há crianças")
+        return
+
+    }
+
+    let lista="" //para concatenar lista de crianças
+    for(const crianca of criancas){
+        const{nome,idade}=crianca // desestrutura do objeto
+        lista+=nome+"-"+idade+"anos\n"
+    }
+    resp.innerText=lista //exibe a lista
+
+    
+
+
+
+})                                                                                                                                 
